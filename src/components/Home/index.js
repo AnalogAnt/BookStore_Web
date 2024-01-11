77fd7b7c41c17820d51a4daf3e66c6d9191b2178
Vikas Bagalkot <vikasbagalkot04@gmail.com>
2024-01-11 23:48:34 +0530

    Add static version of Home and Header components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,28 @@
 import { Component } from "react";
-
+import Header from "../Header"
+import "./index.css"
 class Home extends Component
 {
     render()
     {
         return(
-            <div>
-                Hi
-            </div>
+            <><Header /><div className="banner-container">
+                <div className="banner-content-container">
+                    <h1 className="banner-heading">Book Store</h1>
+                    <p className="banner-text">
+                        Bookstore was founded in 1992 with a simple yet passionate
+                        mission to positively impact the world through the power of
+                        reading. Right from our first store in Mumbai to the 92 stores.
+                    </p>
+                    <p className="banner-text">
+                        As India's leading Bookstore retialer, we champion books and
+                        nourish a love for the written word through a rich, handpicked
+                        collective dfadlkf a df a l fd afd kdsf a dkafjldk lkfdka  kd
+                        kfjdla  kfdjalef jkja f8rh ta dsfaj j ieuf j.
+                    </p>
+                    <button className="explore-button">Explore Books</button>
+                </div>
+            </div></>
         );
     }
 }
